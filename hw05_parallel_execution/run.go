aa8a8012b53dbddb5d39cec85a1ec1ec718623db
@danilko.a.g <danilko.a.g@gmail.com>
2025-10-15 01:13:33 +0300

    prepare func run and add main for test

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,7 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"log"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +11,38 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
+
+	log.Printf("Starting: %d tasks, %d workers, %d max errors", len(tasks), n, m)
+
+	taskTests := []Task{
+		func() error {
+			return nil
+		},
+		func() error {
+			return nil
+		},
+		func() error {
+			return nil
+		},
+	}
+
+	_ = tasks
+	_ = taskTests
+
 	// Place your code here.
 	return nil
+}
+
+func СreateTestTasks() []Task {
+	return []Task{
+		func() error {
+			return nil
+		},
+		func() error {
+			return nil
+		},
+		func() error {
+			return nil
+		},
+	}
 }
