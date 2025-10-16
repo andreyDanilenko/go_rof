476974a61fe300fe6a6acf707880ae7c85c68a7c
@danilko.a.g <danilko.a.g@gmail.com>
2025-10-16 12:41:37 +0300

    add goroutines

--- hw05_parallel_execution/cmd/main.go
+++ hw05_parallel_execution/cmd/main.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	"log"
-
 	exec "github.com/fixme_my_friend/hw05_parallel_execution"
 )
 
 func main() {
-	tasksTest := exec.СreateTestTasks()
-	n := 1
+	tasksTest := exec.CreateTestTasks()
+	n := 3
 	m := 3
-	log.Printf("Starting: %d tasks, %d workers, %d max errors", len(tasksTest), n, m)
-
 	err := exec.Run(tasksTest, n, m)
-
 	_ = err
-
-	// Temporary fix to use the variable
 
 }
--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -3,6 +3,7 @@ package hw05parallelexecution
 import (
 	"errors"
 	"log"
+	"time"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -11,37 +12,54 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-
 	log.Printf("Starting: %d tasks, %d workers, %d max errors", len(tasks), n, m)
 
-	taskTests := []Task{
-		func() error {
-			return nil
-		},
-		func() error {
-			return nil
-		},
-		func() error {
-			return nil
-		},
+	tasksChan := make(chan Task)
+
+	// Запускаем n воркеров
+	for i := 0; i < n; i++ {
+		workerID := i + 1
+		go func() {
+			log.Printf("Worker %d: запущен и ждет задачи...", workerID)
+
+			for task := range tasksChan {
+				log.Printf("Worker %d: получил задачу", workerID)
+				err := task()
+				if err != nil {
+					log.Printf("Worker %d: ошибка - %v", workerID, err)
+				} else {
+					log.Printf("Worker %d: задача выполнена успешно", workerID)
+				}
+			}
+
+			log.Printf("Worker %d: завершил работу", workerID)
+		}()
 	}
 
-	_ = tasks
-	_ = taskTests
+	// Отправляем задачи в канал с номерами
+	log.Printf("Отправляю задачи в канал...")
+	for i, task := range tasks {
+		taskNumber := i + 1
+		log.Printf("Отправляю задачу %d в канал", taskNumber)
+		tasksChan <- task
+	}
 
-	// Place your code here.
+	log.Printf("Все задачи отправлены, закрываю канал...")
+	close(tasksChan)
+
+	time.Sleep(1 * time.Second)
+	log.Printf("Run завершен")
 	return nil
 }
 
-func СreateTestTasks() []Task {
+func CreateTestTasks() []Task {
 	return []Task{
 		func() error {
+			log.Printf("Задача 1 выполняется")
 			return nil
 		},
 		func() error {
-			return nil
-		},
-		func() error {
+			log.Printf("Задача 2 выполняется")
 			return nil
 		},
 	}
