b4089b7358d33ab6b822a929dd3ec1fe875e43f6
@danilko.a.g <danilko.a.g@gmail.com>
2025-10-23 12:49:23 +0300

    WIP

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -10,13 +10,14 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 type Task func() error
 
 func Run(tasks []Task, n, m int) error {
-	tasksChan := make(chan Task, n)
+	tasksChan := make(chan Task)
 
 	var wg sync.WaitGroup
 	var mu sync.Mutex
 	var errCount int
 
 	wg.Add(n)
+
 	for i := 0; i < n; i++ {
 		go func() {
 			defer wg.Done()
