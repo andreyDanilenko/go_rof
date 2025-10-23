5943e4c0c0cc665e7fb59f95be1fe88d825e4839
@danilko.a.g <danilko.a.g@gmail.com>
2025-10-23 12:58:43 +0300

    update code

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -9,48 +9,75 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
 	if n <= 0 {
 		return errors.New("workers count must be > 0")
 	}
 
-	tasksChan := make(chan Task)
+	type taskWithID struct {
+		id   int
+		task Task
+	}
+
+	tasksChan := make(chan taskWithID)
+	errChan := make(chan error)
+	done := make(chan struct{})
 
 	var wg sync.WaitGroup
-	var mu sync.Mutex
 	var errCount int
+
+	worker := func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-done:
+				return
+			case t, ok := <-tasksChan:
+				if !ok {
+					return
+				}
+				select {
+				case errChan <- t.task():
+				case <-done:
+					return
+				}
+			}
+		}
+	}
 
 	wg.Add(n)
 	for i := 0; i < n; i++ {
-		go func() {
-			defer wg.Done()
-			for task := range tasksChan {
-				if err := task(); err != nil {
-					mu.Lock()
-					errCount++
-					mu.Unlock()
+		go worker()
+	}
+
+	go func() {
+		for i, task := range tasks {
+			select {
+			case <-done:
+				return
+			case tasksChan <- taskWithID{i, task}:
+			}
+		}
+		close(tasksChan)
+	}()
+
+	go func() {
+		wg.Wait()
+		close(errChan)
+	}()
+
+	for err := range errChan {
+		if err != nil {
+			if m > 0 {
+				errCount++
+				if errCount >= m {
+					close(done)
+					wg.Wait()
+					return ErrErrorsLimitExceeded
 				}
 			}
-		}()
-	}
-
-	for _, task := range tasks {
-		tasksChan <- task
-
-		mu.Lock()
-		if m > 0 && errCount >= m {
-			mu.Unlock()
-			break
 		}
-		mu.Unlock()
-
-	}
-	close(tasksChan)
-
-	wg.Wait()
-
-	if m > 0 && errCount >= m {
-		return ErrErrorsLimitExceeded
 	}
 
 	return nil
