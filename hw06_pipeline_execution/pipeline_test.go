3607aafb12f9b18babdb24c58b0b91baaa5c9bfc
@danilko.a.g <danilko.a.g@gmail.com>
2025-11-03 21:30:56 +0300

    WIP

--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -67,7 +67,6 @@ func TestPipeline(t *testing.T) {
 		done := make(Bi)
 		data := []int{1, 2, 3, 4, 5}
 
-		// Abort after 200ms
 		abortDur := sleepPerStage * 2
 		go func() {
 			<-time.After(abortDur)
