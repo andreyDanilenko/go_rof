c1c141d3598722ccc0435d9454965bc5ad949ee6
andreyDanilenko <danilenko.a.g@mail.ru>
2026-01-05 21:12:47 +0300

    add tests for copy util

--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 )
 
 var (
@@ -18,5 +19,5 @@ func init() {
 
 func main() {
 	flag.Parse()
-	// Place your code here.
+	fmt.Println(from, to, offset, limit)
 }
